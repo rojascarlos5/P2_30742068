fbb8647e63166d8866053eb0196a16bb8918703c
Carlos Rojas <ggggoglez2@gmail.com>
2022-05-28 17:29:22 -0400

    la nueva version

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express()
 
 app.get('/', function(req, res){
 
-    res.send('Curriculum Vitae CV proximamente')
+    res.send('Curriculum Vitae CV proximamente el guevo pelao')
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT, function() {
 
