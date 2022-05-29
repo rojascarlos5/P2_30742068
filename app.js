745e8e5af46b8dc07ecab85579c96d044ecd993d
Carlos Rojas <ggggoglez2@gmail.com>
2022-05-28 20:37:08 -0400

    Nos habiamos olvidado de estos archivos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
     res.send('Curriculum Vitae CV proximamente el guevo pelao')
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 
 app.listen(PORT, function() {
